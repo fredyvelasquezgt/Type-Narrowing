706576585ae6ccc8d03b4557081e2a0ea14b0011
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-04-12 14:36:18 -0600

    added truthiness guards

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,23 @@ function triple(value: number | string) {
     }
     return value * 3
 }
+
+
+const el = document.getElementById("idk") //aqui puede ser dos 
+if(el) { //aqui ya es puer HTMLElement
+    
+} else {
+    el
+}
+
+const printLetters = (word?:string)=>{
+    if(word) {
+        for(let char of word) {
+            console.log(char);
+        }
+    }
+
+    else {
+        console.log('no es posible pasar');
+    }
+}
