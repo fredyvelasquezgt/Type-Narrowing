b98f0e7e327e16212274943ff16f5624291d6be4
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-04-12 15:20:52 -0600

    added type predicates

--- src/index.ts
+++ src/index.ts
@@ -84,3 +84,30 @@ function printName(entity: User | Company) {
         
     }
 }
+
+//Type Predicates
+
+interface Cat {
+    name:string,
+    numLives: number
+}
+
+interface Dog {
+    name:string,
+    breed:string
+}
+
+function isCat(animal: Cat | Dog): animal is Cat {
+   return (animal as Cat).numLives !== undefined; 
+}
+
+
+function makeNoise(animal: Cat | Dog): string {
+    if(isCat(animal)) {
+        animal
+        return 'meow'
+    } else {
+        animal
+    }
+}
+
